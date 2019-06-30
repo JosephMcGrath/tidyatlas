d238a5e65e4adc1d17d09764967c046da6e7f7c8
Joe McGrath <joseph.mcgrath@outlook.com>
2019-06-30 11:12:35 +0100

    Added table of reference latitude zones

---
+++ sql_templates/global/01_table_definition/03_reference_latitude_zone.sql
@@ -0,0 +1,64 @@
+{% if global %}
+BEGIN;
+
+    CREATE TABLE reference_latitude_zone_input (
+        fid INTEGER PRIMARY KEY
+      , min_latitude REAL
+      , max_latitude REAL
+      , label TEXT
+    );
+
+    CREATE TABLE reference_latitude_zone (
+        fid INTEGER PRIMARY KEY
+      , min_latitude REAL
+      , max_latitude REAL
+      , label TEXT
+      , uuid TEXT NOT NULL
+      , the_geom LINESTRING
+    );
+
+    SELECT
+        RecoverGeometryColumn('reference_latitude_zone',
+                              'the_geom',
+                              4326,
+                              'POLYGON',
+                              'XY'
+                              );
+
+    /*Geometry creation SQL*/
+    CREATE TRIGGER reference_latitude_zone_input_insert
+    AFTER INSERT ON reference_latitude_zone_input
+    BEGIN
+        INSERT INTO reference_latitude_zone (fid, min_latitude, max_latitude, label, uuid, the_geom)
+        SELECT
+            NEW.fid
+          , NEW.min_latitude
+          , NEW.max_latitude
+          , NEW.label
+          , CreateUUID()
+          , ST_Envelope(MakeLine(MakePoint(-180, NEW.min_latitude, 4326),
+                     MakePoint(180, NEW.max_latitude, 4326)
+                 ));
+    END;
+
+    CREATE TRIGGER reference_latitude_zone_input_update
+    AFTER UPDATE ON reference_latitude_zone_input
+    BEGIN
+        UPDATE reference_latitude_zone
+        SET min_latitude = NEW.min_latitude
+          , max_latitude = NEW.max_latitude
+          , label = NEW.label
+          , the_geom = ST_Envelope(MakeLine(MakePoint(-180, NEW.min_latitude, 4326),
+                                MakePoint(180, NEW.max_latitude, 4326)
+                            ))
+        WHERE fid = OLD.fid;
+    END;
+
+    CREATE TRIGGER reference_latitude_zone_input_delete
+    AFTER DELETE ON reference_latitude_zone_input
+    BEGIN
+        DELETE FROM reference_latitude_zone WHERE fid = OLD.fid;
+    END;
+
+COMMIT;
+{% endif %}
