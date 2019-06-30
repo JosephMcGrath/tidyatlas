f6441c9f74b84bf6d92d48d84149e5bb0befbfcf
Joe McGrath <joseph.mcgrath@outlook.com>
2019-06-30 10:23:42 +0100

    Added definition for world bounding box.

---
+++ sql_templates/global/01_table_definition/01_world_boundary.sql
@@ -0,0 +1,18 @@
+BEGIN;
+
+    /*World bounding box.*/
+    CREATE TABLE world_boundary (
+        fid INTEGER PRIMARY KEY
+      , uuid TEXT NOT NULL
+      , the_geom POLYGON UNIQUE
+    );
+
+    SELECT
+        RecoverGeometryColumn('worldworld_boundary_box',
+                              'the_geom',
+                              4326,
+                              'POLYGON',
+                              'XY'
+                              );
+
+COMMIT;
---
+++ sql_templates/global/03_data_generation/01_world_boundary.sql
@@ -0,0 +1,5 @@
+BEGIN;
+    /*World bounding box*/
+    INSERT OR IGNORE INTO world_boundary (uuid, the_geom)
+    VALUES(CreateUUID(), ST_GeomFromText('POLYGON ((-180 90, 180 90, 180 -90, -180 -90, -180 90))', 4326));
+COMMIT;
