f17e6e920bdcc423f7f04536deae5e841054f8d3
Joe McGrath <joseph.mcgrath@outlook.com>
2019-06-30 14:39:08 +0100

    Fixed refrence table not being used correctly

--- sql_templates/global/01_table_definition/04_tectonic_plate.sql
+++ sql_templates/global/01_table_definition/04_tectonic_plate.sql
@@ -15,7 +15,7 @@ BEGIN;
     /*World bounding box.*/
     CREATE TABLE tectonic_plate (
         fid INTEGER PRIMARY KEY
-      , category TEXT REFERENCES tectonic_plate_category
+      , category TEXT REFERENCES tectonic_plate_category (category)
       , uuid TEXT NOT NULL
       , created TEXT
       , modified TEXT
