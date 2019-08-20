fd7bcd5d39d1ad4e9df1afa9a8daaf7dc1f225b3
Joe McGrath <joseph.mcgrath@outlook.com>
2019-08-20 19:53:30 +0100

    Importing previous linear labels.

--- sql_templates/03_local/02_data_import/import_previous.sql
+++ sql_templates/03_local/02_data_import/import_previous.sql
@@ -78,6 +78,13 @@ SELECT
     source, destination, typical_duration, uuid, line_length, created, modified, the_geom
 FROM previous.shipping;
 
+/*Linear Labels*/
+INSERT INTO linear_label
+    (label, style, uuid, line_length, created, modified, the_geom)
+SELECT
+    label, style, uuid, line_length, created, modified, the_geom
+FROM previous.linear_label;
+
 
 COMMIT;
 
