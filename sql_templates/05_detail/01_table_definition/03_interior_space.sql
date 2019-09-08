c24f97b28f608cb1092b21b0e5678803a87718a9
Joe McGrath <joseph.mcgrath@outlook.com>
2019-09-08 09:49:05 +0100

    Imported previous space layer definition from previous database design.

---
+++ sql_templates/05_detail/01_table_definition/03_interior_space.sql
@@ -0,0 +1,72 @@
+{% if detail %}
+BEGIN;
+
+    CREATE TABLE tile_types (
+        type TEXT
+    );
+
+    INSERT INTO tile_types (type)
+    VALUES
+        ("Plain")
+      , ("Wall")
+      , ("Open Air")
+      , ("Water")
+      , ("Planks")
+      , ("Structure")
+      , ("Furniture")
+      , ('Tiles');
+
+{% for floor in floors %}
+    CREATE TABLE IF NOT EXISTS interior_space_f_{{floor.label}} (
+        fid INTEGER PRIMARY KEY AUTOINCREMENT
+      , name TEXT
+      , floor_pattern TEXT /*REFERENCES tile_types(type)*/
+      , colour_name TEXT
+      , colour_hex TEXT
+      , colour_lightness REAL DEFAULT 0
+      , colour_override TEXT
+      , area REAL
+      , uuid TEXT NOT NULL UNIQUE
+      , created TEXT NOT NULL
+      , edited TEXT NOT NULL
+      , the_geom MULTIPOLYGON
+    );
+
+    SELECT
+        RecoverGeometryColumn('interior_space_f_{{floor.label}}',
+                              'the_geom',
+                              {{local_datum}},
+                              'MULTIPOLYGON',
+                              'XY'
+                              )
+      , CreateSpatialIndex('interior_space_f_{{floor.label}}', 'the_geom');
+
+    CREATE TRIGGER interior_space_f_{{floor.label}}_insert AFTER INSERT ON interior_space_f_{{floor.label}}
+    BEGIN
+        UPDATE interior_space_f_{{floor.label}}
+        SET area = ST_Area(the_geom)
+          , the_geom = COALESCE(MakeValid(the_geom), the_geom)
+        WHERE fid = NEW.fid;
+
+        UPDATE interior_space_f_{{floor.label}}
+        SET colour_hex = (SELECT colour_hex FROM colour_pallete WHERE colour_name = NEW.colour_name)
+        WHERE fid = NEW.fid
+          AND NEW.colour_name IS NOT NULL;
+    END;
+
+    CREATE TRIGGER interior_space_f_{{floor.label}}_update AFTER UPDATE ON interior_space_f_{{floor.label}}
+    BEGIN
+        UPDATE interior_space_f_{{floor.label}}
+        SET area = ST_Area(the_geom)
+          , the_geom = COALESCE(MakeValid(the_geom), the_geom)
+        WHERE fid = NEW.fid;
+
+        UPDATE interior_space_f_{{floor.label}}
+        SET colour_hex = (SELECT colour_hex FROM colour_pallete WHERE colour_name = NEW.colour_name)
+        WHERE fid = NEW.fid
+          AND NEW.colour_name IS NOT NULL;
+    END;
+{% endfor %}
+
+COMMIT;
+{% endif %}
---
+++ sql_templates/05_detail/01_table_definition/04_interior_feature.sql
@@ -0,0 +1,52 @@
+{% if detail %}
+BEGIN;
+
+    CREATE TABLE interior_feature_type (
+        type TEXT
+    );
+
+    INSERT INTO interior_feature_type (type)
+    VALUES
+        ("Door")
+      , ("Door (locked)")
+      , ("Step")
+      , ("Wall")
+      ;
+
+{% for floor in floors %}
+    CREATE TABLE IF NOT EXISTS interior_feature_f_{{floor.label}} (
+        fid INTEGER PRIMARY KEY AUTOINCREMENT
+      , type TEXT /*REFERENCES interior_feature_f_{{floor.label}}_type(type)*/
+      , length REAL
+      , uuid TEXT NOT NULL UNIQUE
+      , created TEXT NOT NULL
+      , edited TEXT NOT NULL
+      , the_geom LINESTRING
+    );
+
+    SELECT
+        RecoverGeometryColumn('interior_feature_f_{{floor.label}}',
+                              'the_geom',
+                              {{local_datum}},
+                              'LINESTRING',
+                              'XY'
+                              )
+      , CreateSpatialIndex('interior_feature_f_{{floor.label}}', 'the_geom');
+
+    CREATE TRIGGER interior_feature_f_{{floor.label}}_insert AFTER INSERT ON interior_feature_f_{{floor.label}}
+    BEGIN
+        UPDATE interior_feature_f_{{floor.label}}
+        SET length = ST_Length(the_geom)
+        WHERE fid = NEW.fid;
+    END;
+
+    CREATE TRIGGER interior_feature_f_{{floor.label}}_update AFTER UPDATE ON interior_feature_f_{{floor.label}}
+    BEGIN
+        UPDATE interior_feature_f_{{floor.label}}
+        SET length = ST_Length(the_geom)
+        WHERE fid = NEW.fid;
+    END;
+{% endfor %}
+
+COMMIT;
+{% endif %}
