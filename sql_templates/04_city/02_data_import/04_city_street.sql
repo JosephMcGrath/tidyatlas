c94e2775d1fa80628630c5ef7f00da38bf631adc
Joe McGrath <joseph.mcgrath@outlook.com>
2019-10-05 11:57:55 +0100

    Corrected an error in the city street import script.

--- sql_templates/04_city/02_data_import/04_city_street.sql
+++ sql_templates/04_city/02_data_import/04_city_street.sql
@@ -8,7 +8,7 @@ INSERT INTO city_street
     (name, width, uuid, line_length, created, modified, the_geom)
 SELECT
     name, width, uuid, line_length, created, modified, the_geom
-FROM previous.city_wall;
+FROM previous.city_street;
 
 COMMIT;
 
