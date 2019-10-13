aa44bf4f901cae5a64b676ade16997beea3b67af
Joe McGrath <joseph.mcgrath@outlook.com>
2019-10-13 11:13:49 +0100

    Added colour fields to interior features.

--- sql_templates/05_detail/01_table_definition/04_interior_feature.sql
+++ sql_templates/05_detail/01_table_definition/04_interior_feature.sql
@@ -17,6 +17,12 @@ BEGIN;
     CREATE TABLE IF NOT EXISTS interior_feature_f_{{floor.label}} (
         fid INTEGER PRIMARY KEY AUTOINCREMENT
       , type TEXT NOT NULL REFERENCES interior_feature_type(type)
+      , colour_name TEXT
+      , colour_primary_hex TEXT
+      , colour_primary_override TEXT
+      , colour_secondary_hex TEXT
+      , colour_secondary_override TEXT
+      , colour_lightness REAL DEFAULT 0
       , length REAL
       , uuid TEXT NOT NULL UNIQUE
       , created TEXT NOT NULL
@@ -38,6 +44,12 @@ BEGIN;
         UPDATE interior_feature_f_{{floor.label}}
         SET length = ST_Length(the_geom)
         WHERE fid = NEW.fid;
+
+        UPDATE interior_feature_f_{{floor.label}}
+        SET colour_primary_hex = (SELECT primary_colour FROM colour_pallete WHERE colour_name = NEW.colour_name)
+          , colour_secondary_hex = (SELECT secondary_colour FROM colour_pallete WHERE colour_name = NEW.colour_name)
+        WHERE fid = NEW.fid
+          AND NEW.colour_name IS NOT NULL;
     END;
 
     CREATE TRIGGER interior_feature_f_{{floor.label}}_update AFTER UPDATE ON interior_feature_f_{{floor.label}}
@@ -45,6 +57,28 @@ BEGIN;
         UPDATE interior_feature_f_{{floor.label}}
         SET length = ST_Length(the_geom)
         WHERE fid = NEW.fid;
+
+        UPDATE interior_feature_f_{{floor.label}}
+        SET colour_primary_hex = (SELECT primary_colour FROM colour_pallete WHERE colour_name = NEW.colour_name)
+          , colour_secondary_hex = (SELECT secondary_colour FROM colour_pallete WHERE colour_name = NEW.colour_name)
+        WHERE fid = NEW.fid
+          AND NEW.colour_name IS NOT NULL;
+    END;
+
+    CREATE TRIGGER colour_pallete_insert_interior_feature_f_{{floor.label}} AFTER INSERT ON colour_pallete
+    BEGIN
+        UPDATE interior_feature_f_{{floor.label}}
+        SET colour_primary_hex = NEW.primary_colour
+          , colour_secondary_hex = NEW.secondary_colour
+        WHERE colour_name = NEW.colour_name;
+    END;
+
+    CREATE TRIGGER colour_pallete_update_interior_feature_f_{{floor.label}} AFTER UPDATE ON colour_pallete
+    BEGIN
+        UPDATE interior_feature_f_{{floor.label}}
+        SET colour_primary_hex = NEW.primary_colour
+          , colour_secondary_hex = NEW.secondary_colour
+        WHERE colour_name = NEW.colour_name;
     END;
 {% endfor %}
 
