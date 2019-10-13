00d8b5a7c4b1f125148dc9c82d7f2e2320c18d33
Joe McGrath <joseph.mcgrath@outlook.com>
2019-10-13 16:35:21 +0100

    Added exterior feature layer definition.

---
+++ sql_templates/05_detail/01_table_definition/02_exterior_feature.sql
@@ -0,0 +1,84 @@
+{% if detail %}
+BEGIN;
+
+    CREATE TABLE exterior_feature_type (
+        type TEXT
+    );
+
+    INSERT INTO exterior_feature_type (type)
+    VALUES
+        ("Door")
+      , ("Door (locked)")
+      , ("Step")
+      , ("Wall")
+      ;
+
+    CREATE TABLE IF NOT EXISTS exterior_feature (
+        fid INTEGER PRIMARY KEY AUTOINCREMENT
+      , type TEXT NOT NULL REFERENCES exterior_feature_type(type)
+      , colour_name TEXT
+      , colour_primary_hex TEXT
+      , colour_primary_override TEXT
+      , colour_secondary_hex TEXT
+      , colour_secondary_override TEXT
+      , colour_lightness REAL DEFAULT 0
+      , length REAL
+      , uuid TEXT NOT NULL UNIQUE
+      , created TEXT NOT NULL
+      , edited TEXT NOT NULL
+      , the_geom LINESTRING
+    );
+
+    SELECT
+        RecoverGeometryColumn('exterior_feature',
+                              'the_geom',
+                              {{local_datum}},
+                              'LINESTRING',
+                              'XY'
+                              )
+      , CreateSpatialIndex('exterior_feature', 'the_geom');
+
+    CREATE TRIGGER exterior_feature_insert AFTER INSERT ON exterior_feature
+    BEGIN
+        UPDATE exterior_feature
+        SET length = ST_Length(the_geom)
+        WHERE fid = NEW.fid;
+
+        UPDATE exterior_feature
+        SET colour_primary_hex = (SELECT primary_colour FROM colour_pallete WHERE colour_name = NEW.colour_name)
+          , colour_secondary_hex = (SELECT secondary_colour FROM colour_pallete WHERE colour_name = NEW.colour_name)
+        WHERE fid = NEW.fid
+          AND NEW.colour_name IS NOT NULL;
+    END;
+
+    CREATE TRIGGER exterior_feature_update AFTER UPDATE ON exterior_feature
+    BEGIN
+        UPDATE exterior_feature
+        SET length = ST_Length(the_geom)
+        WHERE fid = NEW.fid;
+
+        UPDATE exterior_feature
+        SET colour_primary_hex = (SELECT primary_colour FROM colour_pallete WHERE colour_name = NEW.colour_name)
+          , colour_secondary_hex = (SELECT secondary_colour FROM colour_pallete WHERE colour_name = NEW.colour_name)
+        WHERE fid = NEW.fid
+          AND NEW.colour_name IS NOT NULL;
+    END;
+
+    CREATE TRIGGER colour_pallete_insert_exterior_feature AFTER INSERT ON colour_pallete
+    BEGIN
+        UPDATE exterior_feature
+        SET colour_primary_hex = NEW.primary_colour
+          , colour_secondary_hex = NEW.secondary_colour
+        WHERE colour_name = NEW.colour_name;
+    END;
+
+    CREATE TRIGGER colour_pallete_update_exterior_feature AFTER UPDATE ON colour_pallete
+    BEGIN
+        UPDATE exterior_feature
+        SET colour_primary_hex = NEW.primary_colour
+          , colour_secondary_hex = NEW.secondary_colour
+        WHERE colour_name = NEW.colour_name;
+    END;
+
+COMMIT;
+{% endif %}
