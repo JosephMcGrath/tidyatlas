690f2b87dfdbf3ac92d1109b3cc9001cb3e8485b
Joe McGrath <joseph.mcgrath@outlook.com>
2019-10-17 19:56:01 +0100

    Added a 'nation' level of political boundaries.

--- sql_templates/03_local/01_table_definition/10_political_region.sql
+++ sql_templates/03_local/01_table_definition/10_political_region.sql
@@ -1,11 +1,48 @@
 {% if local %}
 BEGIN;
 
+    /*Nation-scale regions.*/
+    CREATE TABLE IF NOT EXISTS political_nation (
+        fid INTEGER PRIMARY KEY AUTOINCREMENT
+      , name TEXT
+      , notes TEXT
+      , colour TEXT
+      , uuid TEXT NOT NULL UNIQUE
+      , area REAL
+      , created TEXT NOT NULL
+      , modified TEXT NOT NULL
+      , the_geom MULTIPOLYGON
+    );
+
+    SELECT
+      RecoverGeometryColumn('political_nation',
+                            'the_geom',
+                            {{local_datum}},
+                            'MULTIPOLYGON',
+                            'XY'
+                            )
+    , CreateSpatialIndex('political_nation', 'the_geom');
+
+    CREATE TRIGGER political_nation_insert AFTER INSERT ON political_nation
+    BEGIN
+      UPDATE political_nation
+      SET area = ST_Area(the_geom)
+      WHERE fid = NEW.fid;
+    END;
+
+    CREATE TRIGGER political_nation_update AFTER UPDATE ON political_nation
+    BEGIN
+      UPDATE political_nation
+      SET area = ST_Area(the_geom)
+      WHERE fid = NEW.fid;
+    END;
+
     /*Political regions.*/
     CREATE TABLE IF NOT EXISTS political_region (
         fid INTEGER PRIMARY KEY AUTOINCREMENT
       , name TEXT
       , notes TEXT
+      , colour TEXT
       , uuid TEXT NOT NULL UNIQUE
       , area REAL
       , created TEXT NOT NULL
