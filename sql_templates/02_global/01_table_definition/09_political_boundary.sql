818bccdfd27a41d763ff6114f221236a2ecb6c40
Joe McGrath <joseph.mcgrath@outlook.com>
2019-12-14 10:28:36 +0000

    Parameterised global political boundary tables.

--- sql_templates/02_global/01_table_definition/09_political_boundary.sql
+++ sql_templates/02_global/01_table_definition/09_political_boundary.sql
@@ -1,7 +1,8 @@
 {% extends "base.sql" %}
 {% block content %}
     /*Boundaries (Political)*/
-    CREATE TABLE major_political_boundary (
+{% for table_name in ["major_political_boundary", "minor_political_boundary"] %}
+    CREATE TABLE {{table_name}} (
         fid INTEGER PRIMARY KEY
       , name TEXT
       , colour TEXT
@@ -12,28 +13,11 @@
     );
 
     SELECT
-        RecoverGeometryColumn('major_political_boundary',
+        RecoverGeometryColumn('{{table_name}}',
                               'the_geom',
                               4326,
                               'MULTIPOLYGON',
                               'XY'
                               );
-
-    CREATE TABLE minor_political_boundary (
-        fid INTEGER PRIMARY KEY
-      , name TEXT
-      , colour TEXT
-      , uuid TEXT NOT NULL
-      , created TEXT
-      , modified TEXT
-      , the_geom MULTIPOLYGON
-    );
-
-    SELECT
-        RecoverGeometryColumn('minor_political_boundary',
-                              'the_geom',
-                              4326,
-                              'MULTIPOLYGON',
-                              'XY'
-                              );
+{% endfor %}
 {% endblock %}
