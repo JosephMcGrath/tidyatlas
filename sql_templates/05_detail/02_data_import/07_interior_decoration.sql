fbd54d0fd35cabd8cb646dd511261c4a8db4b265
Joe McGrath <joseph.mcgrath@outlook.com>
2020-01-27 21:33:22 +0000

    Corrected interior decoration import statement.

--- sql_templates/05_detail/02_data_import/07_interior_decoration.sql
+++ sql_templates/05_detail/02_data_import/07_interior_decoration.sql
@@ -1,10 +1,10 @@
 {% extends "base.sql" %}
 {% block content %}
 {% for floor in floors %}
-INSERT INTO interior_decoration_f_f_{{floor.label}}
+INSERT INTO interior_decoration_f_{{floor.label}}
     (asset_path, size, rotation, draw_order, opacity, uuid, created, modified, the_geom)
 SELECT
     asset_path, size, rotation, draw_order, opacity, uuid, created, modified, the_geom
-FROM previous.interior_decoration_f_f_{{floor.label}};
+FROM previous.interior_decoration_f_{{floor.label}};
 {% endfor %}
 {% endblock %}
