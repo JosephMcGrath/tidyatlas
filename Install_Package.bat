6ef5019ae1a493bffa88e1fff6fe6f2bbc900334
Joe McGrath <joseph.mcgrath@outlook.com>
2020-01-28 11:17:14 +0000

    Updated the build script for more iterative instalations.

--- Install_Package.bat
+++ Install_Package.bat
@@ -3,6 +3,10 @@
 REM @DEL %~dp0\dist /Q
 
 @python setup.py sdist bdist_wheel > build.log
-@pip install --no-index --find-links=file:%~dp0\dist tidycartographer >> build.log --upgrade
+@ECHO ============================================================================================================================= >> build.log
+@pip uninstall tidycartographer -y >> build.log
+@ECHO ============================================================================================================================= >> build.log
+@pip install --no-index --find-links=file:%~dp0\dist tidycartographer >> build.log
 
+@ECHO Done
 @PAUSE
