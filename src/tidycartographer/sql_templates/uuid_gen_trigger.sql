59a162bfc86506ca4f04879f4343ca2479f6d029
Joe McGrath <joseph.mcgrath@outlook.com>
2020-01-30 11:06:35 +0000

    Updated UUID generate to conflict less with QGIS feature merging.

--- src/tidycartographer/sql_templates/uuid_gen_trigger.sql
+++ src/tidycartographer/sql_templates/uuid_gen_trigger.sql
@@ -3,5 +3,9 @@ CREATE TRIGGER {{table_name}}_uuid_insert AFTER INSERT ON {{table_name}}
 BEGIN
     UPDATE {{table_name}}
     SET uuid = CreateUUID()
-    WHERE fid = NEW.fid;
+    WHERE fid = NEW.fid
+      AND 1 < (SELECT COUNT(*) FROM {{table_name}} WHERE uuid = NEW.uuid)
+    ;
 END;
+
+CREATE INDEX IF NOT EXISTS {{table_name}}_uuid_idx ON {{table_name}} (uuid);
