c91444d3d823f7baa5ee32dff612fe91f1f76aa1
Joe McGrath <joseph.mcgrath@outlook.com>
2020-03-27 21:19:10 +0000

    Added global variants to area/length calc triggers

--- src/tidycartographer/sql_templates/defs/area_calc_trigger.sql
+++ src/tidycartographer/sql_templates/defs/area_calc_trigger.sql
@@ -2,6 +2,24 @@
 INSERT OR ABORT INTO build_log (event)
 VALUES ('Adding area calculation trigger to: {{table_name}}.');
 
+{% if (srid or 4326) == 4326 %}
+
+CREATE TRIGGER {{table_name}}_area_insert AFTER INSERT ON {{table_name}}
+BEGIN
+    UPDATE {{table_name}}
+    SET area = ST_Translate(ST_Area(the_geom), 3857)
+    WHERE fid = NEW.fid;
+END;
+
+CREATE TRIGGER {{table_name}}_area_update AFTER UPDATE ON {{table_name}}
+BEGIN
+    UPDATE {{table_name}}
+    SET area = ST_Translate(ST_Area(the_geom), 3857)
+    WHERE fid = NEW.fid;
+END;
+
+{% else %}
+
 CREATE TRIGGER {{table_name}}_area_insert AFTER INSERT ON {{table_name}}
 BEGIN
     UPDATE {{table_name}}
@@ -15,3 +33,5 @@ BEGIN
     SET area = ST_Area(the_geom)
     WHERE fid = NEW.fid;
 END;
+
+{% endif %}
--- src/tidycartographer/sql_templates/defs/length_calc_trigger.sql
+++ src/tidycartographer/sql_templates/defs/length_calc_trigger.sql
@@ -2,6 +2,24 @@
 INSERT OR ABORT INTO build_log (event)
 VALUES ('Adding length calculation trigger to: {{table_name}}.');
 
+{% if (srid or 4326) == 4326 %}
+
+CREATE TRIGGER {{table_name}}_length_insert AFTER INSERT ON {{table_name}}
+BEGIN
+    UPDATE {{table_name}}
+    SET area = ST_Translate(ST_Length(the_geom), 3857)
+    WHERE fid = NEW.fid;
+END;
+
+CREATE TRIGGER {{table_name}}_length_update AFTER UPDATE ON {{table_name}}
+BEGIN
+    UPDATE {{table_name}}
+    SET area = ST_Translate(ST_Length(the_geom), 3857)
+    WHERE fid = NEW.fid;
+END;
+
+{% else %}
+
 CREATE TRIGGER {{table_name}}_length_insert AFTER INSERT ON {{table_name}}
 BEGIN
     UPDATE {{table_name}}
@@ -15,3 +33,5 @@ BEGIN
     SET line_length = ST_Length(the_geom)
     WHERE fid = NEW.fid;
 END;
+
+{% endif %}
